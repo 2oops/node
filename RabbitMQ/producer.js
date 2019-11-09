61651e327e01fb563c5d457c2ef4e7be0cd5ac52
2oops <yxlaimee@163.com>
2019-11-09 22:47:14 +0800

    node rabbitmq

---
+++ RabbitMQ/producer.js
@@ -0,0 +1,25 @@
+/**
+ * 路由一个死信队列
+ */
+async function producerDLX(connnection) {
+  const testExchange = 'testEx';
+  const testQueue = 'testQu';
+  const testExchangeDLX = 'testExDLX';
+  const testRoutingKeyDLX = 'testRoutingKeyDLX';
+
+  const ch = await connnection.createChannel();
+  await ch.assertExchange(testExchange, 'direct', { durable: true });
+  const queueResult = await ch.assertQueue(testQueue, {
+    exclusive: false,
+    deadLetterExchange: testExchangeDLX,
+    deadLetterRoutingKey: testRoutingKeyDLX,
+  });
+  await ch.bindQueue(queueResult.queue, testExchange);
+  const msg = 'hello world!';
+  console.log('producer msg：', msg);
+  await ch.sendToQueue(queueResult.queue, new Buffer(msg), {
+    expiration: '10000'
+  });
+
+  ch.close();
+}
